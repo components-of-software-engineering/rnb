001cfd2da38de3438b8e9e6c2b2998bb1d37a2d0
Arkadiy <arkadiy@ex.ua>
2020-05-10 12:25:50 +0300

    init client part

---
+++ src/client/webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path'),
+    HtmlWebPackPlugin = require("html-webpack-plugin"),
+    MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: {
+        bundle: [
+            path.join(__dirname, './js/index.jsx'),
+            path.join(__dirname, './css/index.scss')
+        ]
+    },
+    output: {
+        publicPath: '/',
+    },
+    devtool: "sourcemap",
+    module: {
+        rules: [
+            {
+                test: /\.js|.jsx$/,
+                include: path.join(__dirname, './js'),
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.html$/,
+                use: {
+                    loader: "html-loader"
+                }
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    {
+                        loader: 'style-loader',
+                    },
+                    MiniCssExtractPlugin.loader, 
+                    {
+                        loader: 'css-loader',
+                    }, 
+                    {
+                        loader: 'sass-loader'
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: function () {
+                                return [
+                                    require('precss'),
+                                    require('autoprefixer')
+                                ];
+                            }
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: path.join(__dirname, "./html/index.html"),
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
+    ]
+};
