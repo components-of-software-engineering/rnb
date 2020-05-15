b46e75c4601bea1e8b83cb5cfb49fc32914b2cc3
Danya <31900270+danya-psch@users.noreply.github.com>
2020-05-15 04:09:33 +0300

    Fix windows run scripts (#12)
    * Fix windows run scripts
    
    * dat was deleted
    
    * fix run.prod.bat
    
    Co-authored-by: DanyaPes <31900270+DanyaPes@users.noreply.github.com>

--- run.dev.bat
+++ run.dev.bat
@@ -1,6 +1,5 @@
-
 setlocal
-FOR /F "tokens=*" %%i in ('%~dp0/.env') do SET %%i
+FOR /F "tokens=*" %%i in ('type %~dp0.env') do SET %%i
 docker build --tag rnb-site.dev -f dev.dockerfile %~dp0
-docker run --env-file %~dp0/.env -it -p %PORT%:%PORT% rnb-site.dev
+docker run --env-file %~dp0.env -it -p %PORT%:%PORT% rnb-site.dev
 endlocal
--- run.prod.bat
+++ run.prod.bat
@@ -1,6 +1,5 @@
-
 setlocal
-FOR /F "tokens=*" %%i in ('%~dp0/.env') do SET %%i
-docker build --tag rnb-site.prod -f Dockerfile %~dp0
-docker run --env-file %~dp0/.env -it -p %PORT%:%PORT% rnb-site.prod
+FOR /F "tokens=*" %%i in ('type %~dp0.env') do SET %%i
+docker build --tag rnb-site.prod -f Dockerfile  %~dp0
+docker run --env-file %~dp0.env -it -p %PORT%:%PORT% rnb-site.prod
 endlocal
