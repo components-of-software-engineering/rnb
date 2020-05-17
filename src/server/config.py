dbdf0e08184032bd20966fe165e92806a2bc33d3
Arkadiy <arkadiy@ex.ua>
2020-05-17 14:31:09 +0300

    Configure Docker

--- src/server/config.py
+++ src/server/config.py
@@ -1,17 +1,19 @@
+import os
+
 config_db = {
-    'host': "localhost",
-    'port': "5432",
-    'database': "rnb",
-    'user': "postgres",
-    'password': "reRhsybrcs1038"
+    'host': os.getenv("DB_HOST"),
+    'port': os.getenv("DB_PORT"),
+    'database': os.getenv("DB_DBNAME"),
+    'user': os.getenv("DB_USER"),
+    'password': os.getenv("DB_PASS")
 }
 
 config_jwt = {
-    'JWT_SECRET_KEY': 'super-secret',
+    'JWT_SECRET_KEY': os.getenv("JWT_SECRET"),
     'JWT_REFRESH_COOKIE_PATH': '/api/refresh',
     'JWT_TOKEN_LOCATION': ['cookies']
 }
 
 other_configs = {
-    "pwd_salt": "s0/\/\P4$$w0rD"
+    "pwd_salt": os.getenv("PWD_SALT")
 }
