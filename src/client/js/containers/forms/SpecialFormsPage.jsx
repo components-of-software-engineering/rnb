15bd696546a2bb7ac86f5ef4dc2b0ac61fa45900
FirstDrectar <vasjvasj5@gmail.com>
2020-06-01 00:19:55 +0300

    pull

--- src/client/js/containers/forms/SpecialFormsPage.jsx
+++ src/client/js/containers/forms/SpecialFormsPage.jsx
@@ -1,24 +1,27 @@
-import React, { Component } from 'react';
-import SpecialFormTable from './SpecialFormTable';
-import { Link } from 'react-router-dom';
-
+import React, { Component } from "react";
+import SpecialFormTable from "./SpecialFormTable";
+import { Link } from "react-router-dom";
 
 class SpecialFormsPage extends Component {
-    render() {
-        return (
-            <React.Fragment>
-                <h1>Cписок витрачених бланків</h1>
-                <p>
-                    Ви можете зареєструвати новий звіт витрачання бланків
-                </p>
-                <div className="container mb-2 text-center">
-                    <Link className={`btn btn-primary my-1 btn-md ml-auto`} to="/forms/add" role="button">Додати новий звіт витрачання</Link>
-                </div>
-                <p>Нижче в <i>таблиці</i> наведені всі витрачені бланки, зареєстровані Вами:</p>
-                <SpecialFormTable />
-            </React.Fragment>
-        );
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Cписок довіреностей</h1>
+        <p>Ви можете створити нову довіреність</p>
+        <div className="container mb-2 text-center">
+          <Link
+            className={`btn btn-primary my-1 btn-md ml-auto`}
+            to="/forms/add"
+            role="button"
+          >
+            Створити довіреність
+          </Link>
+        </div>
+
+        <SpecialFormTable />
+      </React.Fragment>
+    );
+  }
 }
 
 export default SpecialFormsPage;
